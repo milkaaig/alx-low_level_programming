22e32dc0311d128711552438b468d9ae498ee9fb
milkaaig <105880326+milkaaig@users.noreply.github.com>
2022-06-07 20:23:08 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print first 98 Fibonacci numbers without using long long, malloc,
+ * main - print first 98 Fibonacci numbers without using long long, malloc,
  * pointers, array/tables, or structures
  * Return: 0
  */
