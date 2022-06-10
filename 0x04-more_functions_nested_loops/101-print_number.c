0014cc7eef9ee71ad912e9b14345a5b7e52fa5cf
milkaaig <105880326+milkaaig@users.noreply.github.com>
2022-06-10 21:40:25 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,29 +7,31 @@
 
 void print_number(int n)
 {
-unsigned int tens, digit, positive = n;
-double t_beg = 1;
 
-if (n == 0)
-_putchar('0');
-else
-{
-if (n < 0)
-{
-positive = n * -1;
-_putchar('-');
-}
+	int d = 1, i = 0, ii = 0;
 
-while (t_beg <= positive)
-t_beg *= 10;
-tens = t_beg / 10;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-while (tens >= 1)
-{
-digit = positive / tens;
-_putchar(digit + '0');
-positive = (positive - (tens * digit));
-tens /= 10;
-}
-}
+	while (n / d != 0)
+	{
+		d *= 10;
+		i++;
+	}
+	d = d / 10;
+
+	while (ii < i)
+	{
+		_putchar('0' + n / d);
+		n = n - (n / d) * d;
+		d = d / 10;
+		ii++;
+	}
+
+	if (i == 0)
+		_putchar('0' + n);
+
 }
