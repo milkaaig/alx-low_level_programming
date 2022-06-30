3ee14830cff97eaa788ff3902aaa6e6ab4b839ab
milkaaig <milkaaigetachew@gmail.com>
2022-06-30 21:57:55 +0300

    dood

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,88 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * no_words - function to calculate number of words
+ * @s: string being passed to check for words
+ *
+ * Return: number of words
+ */
+int no_words(char *s)
+{
+int i, num = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (*s == ' ')
+s++;
+else
+{
+
+for (; s[i] != ' ' && s[i] != '\0'; i++)
+s++;
+num++;
+}
+}
+return (num);
+}
+
+/**
+ * free_everything - frees the memory
+ * @string: pointer values being passed for freeing
+ * @i: counter
+ */
+void free_everything(char **string, int i)
+{
+for (; i > 0;)
+free(string[--i]);
+free(string);
+}
+
+/**
+ * strtow - splits string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings
+ */
+char **strtow(char *str)
+{
+int total = 0, i = 0, j = 0, length = 0;
+char **words, *found_word;
+if (str == 0 || *str == 0)
+return (NULL);
+total = no_words(str);
+if (total == 0)
+return (NULL);
+words = malloc((total + 1) * sizeof(char *));
+if (words == 0)
+return (NULL);
+for (; *str != '\0' &&  i < total;)
+{
+if (*str == ' ')
+str++;
+else
+{
+found_word = str;
+for (; *str != ' ' && *str != '\0'; str++)
+{
+length++;
+}
+words[i] = malloc((length + 1) * sizeof(char));
+if (words[i] == 0)
+{
+free_everything(words, i);
+return (NULL);
+}
+for (; *found_word != ' ' && *found_word != '\0'; found_word++)
+{
+words[i][j] = *found_word;
+j++;
+}
+words[i][j] = '\0';
+i++;
+j = 0;
+length = 0;
+str++;
+}
+}
+return (words);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *concat;
+int i1 = 0, i2 = 0, length1 = 0, length2 = 0;
+
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+while (*(s1 + i1))
+length1++, i1++;
+
+while (*(s2 + i2))
+length2++, i2++;
+length2++;
+
+concat = malloc(sizeof(char) * (length1 + length2));
+
+if (concat == NULL)
+return (NULL);
+
+for (i1 = 0; i1 < length1; i1++)
+*(concat + i1) = *(s1 + i1);
+
+for (i2 = 0; i2 < length2; i1++, i2++)
+*(concat + i1) = *(s2 + i2);
+
+return (concat);
+}
