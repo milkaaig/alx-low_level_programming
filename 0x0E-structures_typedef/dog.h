e51fb184ccd329dd224bd5cfb46b96de016cae11
root <root@Milkaai.localdomain>
2022-07-04 22:49:39 +0300

    sisi

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,34 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *dog_t - typedef og struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ *struct dog - structure
+ *@name: first element
+ *@age: second element
+ *@owner: third element
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+int _strlen(char *s);
+
+char *_strcpy(char *dest, char *src);
+
+void free_dog(dog_t *d);
+
+#endif
