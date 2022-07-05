63be8760cb0109224856eb487f7616159a991a3d
milkaaig <105880326+milkaaig@users.noreply.github.com>
2022-07-05 17:25:19 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  *main - Entry point
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stderr);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",56);
 	return (1);
 }
